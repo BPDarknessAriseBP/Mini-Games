bcf810b68719128b7b1961ac3e134924db4db33c
DarknessArise <leebossgaming@gmail.com>
2014-01-14 21:15:15 -0800

    Wizard Battles

--- Wizard Battles/src/me/Ethan/wizardbattles/Main.java
+++ Wizard Battles/src/me/Ethan/wizardbattles/Main.java
@@ -1,5 +1,22 @@
 package me.Ethan.wizardbattles;
 
-public class Main {
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.java.JavaPlugin;
 
+public class Main extends JavaPlugin{
+	
+	public String Name = ChatColor.GOLD + "[WIZARD BATTLES]";
+	
+	//=====
+	
+	public void onEnable(){
+		getLogger().info(Name + ChatColor.GREEN + " has been enabled");
+	}
+	
+	public void onDisable(){
+		getLogger().info(Name + ChatColor.RED + " has been disabled");
+	}
+	
+	
+	
 }
